bcaad2db4b8099ca304995eedc0629c2d7f00696
Maxim <163451225+VallDrous@users.noreply.github.com>
2026-09-09 14:46:26 +0300

    Delete Pr directory

--- Pr/app/src/main/java/BusinesLogic/Calculate.kt
+++
@@ -1,9 +0,0 @@
-package BusinesLogic
-
-import kotlin.math.sqrt
-
-object Calculate{
-    fun OperatingCurrentCalculate(p: Double, u: Double, cosFi: Double) : Double{
-        return p/(sqrt(3.0) * u * cosFi)
-    }
-}
